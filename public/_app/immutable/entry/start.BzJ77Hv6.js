7e7937d7208d4709b66918a99ae757eeed2f166c
Nico Krause <post@nicokrause.com>
2024-10-08 13:53:00 +0500

    changing to ssr so vercel.json gets used

---
+++ public/_app/immutable/entry/start.BzJ77Hv6.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.Or9jDBdZ.js";export{t as start};
--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -1,4 +1,4 @@
-export const ssr = false;
+export const ssr = true;
 export const prerender = true; //a separate html file will be created for this page inside the public folder!
 export const trailingSlash = 'always' //means an index.html will be created inside of a 'page' subfolder instead of just a page.html
 
